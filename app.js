83897f3e1a854188d4b9f4ffdf9742301c00c9cf
Kgupta200 <164209578+Kgupta200@users.noreply.github.com>
2025-03-26 01:38:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let navLinks=document.getElementById("navLinks");
+
+// showing the menu in media query
+function showMenu(){
+    navLinks.style.right="0";
+}
+
+//hiding the menu in media query
+function hideMenu(){
+    navLinks.style.right="-200px";
+}
